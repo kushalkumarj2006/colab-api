71634f1d76400c1797221bb1881ae8d21559b8f3
KUSHAL KUMAR J <kushalkumarj2006@gmail.com>
2026-07-28 12:52:08 +0530

    Fix missing closing quote in project_id environment variable

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ REMOTE_REDIRECT_URI = "https://sdk.cloud.google.com/applicationdefaultauthcode.h
 CLIENT_CONFIG = {
     "installed": {
         "client_id": os.environ.get("OAUTH_CLIENT_ID"),
-        "project_id": os.environ.get("OAUTH_PROJECT_ID,
+        "project_id": os.environ.get("OAUTH_PROJECT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
